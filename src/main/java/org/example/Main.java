d50ec3797746885522c0142fd9ef7eec19b5fe3b
Kamil Kot <kamilkot55555@gmail.com>
2023-11-29 13:56:08 +0100

    Teraz dodaję liczbę

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("To jest main");
+        System.out.println("A tu dodaję liczbę: " + 5);
 
         }
     }
